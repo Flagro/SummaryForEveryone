37bffb4da1e995e8f548f2cbc3943482d4231351
flagro <antonpgrey@gmail.com>
2023-09-25 12:31:21 +0200

    model agent structure

---
+++ summaryforeveryone/model_agent.py
@@ -0,0 +1,26 @@
+class AudioToTextModel:
+    def __init__(self):
+        pass
+
+
+class LanguageModel:
+    def __init__(self):
+        pass
+
+
+class TranslationModel:
+    def __init__(self):
+        pass
+
+
+class WhisperModel(AudioToTextModel):
+    pass
+
+
+class Llama2Model(LanguageModel):
+    pass
+
+
+class ModelAgent:
+    def __init__(self):
+        pass
